f68b6edb38a50b6def1970df15ac9576b01c827a
amyyan210 <amyyan210@gmail.com>
2017-09-13 16:14:04 -0500

    * Create loading message * Replace loading message with fetched list of movies

--- src/index.js
+++ src/index.js
@@ -1,20 +1,44 @@
 /**
  * es6 modules and imports
  */
+
 import sayHello from './hello';
 sayHello('World');
 
 /**
  * require style imports
  */
-const getMovies = require('./getMovies.js');
 
+const getMovies = require('./getMovies.js');
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
+      $("#main").empty();
+  movies.forEach(({id, title, rating}) => {
+      $("#main").append(`
+        <table>
+          <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Rating</th>
+          </tr>
+          <tr>
+            <td>${id}</td>
+            <td>${title}</td>
+            <td>${rating}</td>
+          </tr>
+        </table>
+    `);
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
+});
+
+fetch('https://api.github.com/users').then((response) => {
+    const users = response.json().then((users) => {
+        users.forEach((user) => {
+            // do something with each user object...
+        });
+    });
 });
