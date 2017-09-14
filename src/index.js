11d647d371a9652a50e6cf3d090e3748297027e8
amyyan210 <amyyan210@gmail.com>
2017-09-14 11:24:21 -0500

    * Add functionality to + button to show modal input form

--- src/index.js
+++ src/index.js
@@ -8,13 +8,14 @@ sayHello('World');
 /**
  * require style imports
  */
+
 const getMovies = require('./getMovies.js');
-const modalForms = require('./modal.js');
+const modalEvents = require('./modal.js');
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
       $("#main").empty();
-      let table = "<h2 id='title'>Movies </h2><span class='glyphicon glyphicon-plus'></span><table><tr><th>ID</th><th>Title</th><th>Rating</th></tr>";
+      let table = "<h2 id='title'>Movies </h2><span class='glyphicon glyphicon-plus' id='show-add-form' data-target='#add-movie-form' data-toggle='modal'></span><table><tr><th>ID</th><th>Title</th><th>Rating</th></tr>";
 
       movies.forEach(({id, title, rating}) => {
           table += `
@@ -27,6 +28,9 @@ getMovies().then((movies) => {
     });
         table += `</table>`;
     $("#main").append(table);
+
+    modalEvents.modalForms();
+
   }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
